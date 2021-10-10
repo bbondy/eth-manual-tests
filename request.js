78bb5756c32d780babcb94b4685e8c31077254f4
Brian R. Bondy <netzen@gmail.com>
2021-10-09 23:27:44 -0400

    Add tests for enable, adding ethereum chains, and more

--- request.js
+++ request.js
@@ -1,30 +1,46 @@
-function sendAsync(method) {
+const addEthereumChainParams = [{
+  chainId: '0x38',
+  chainName: 'Binance1 Smart Chain',
+  nativeCurrency: {
+  name: 'BNB',
+  symbol: 'BNB',
+  decimals: 18
+},
+rpcUrls: ['https://bsc-dataseed.binance.org/'],
+  iconUrls: [],
+  blockExplorerUrls: ['https://bscscan.com/'],
+}]
+
+function sendAsync(method, params=[]) {
   console.log('sendAsync...')
   window.ethereum.sendAsync({
     id: 'hello',
     method,
-    params: [],
+    params,
   }, (a, b) => { 
     console.log(a, b) 
   })
 }
 
-function send(method) {
+function send(method, params=[]) {
   console.log('send...')
   window.ethereum.send({
     id: 'hello2',
     method,
-    params: [],
+    params,
   }, (a, b) => { 
     console.log(a, b) 
   })
 }
 
-async function request(method) {
+async function request(method, params=[]) {
   console.log(await window.ethereum.request({
     id: '191',
     method,
-    params: [],
+    params,
   }))
 }
 
+async function enable() {
+  console.log(await window.ethereum.enable())
+}
